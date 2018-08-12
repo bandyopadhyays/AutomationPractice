0b24d4f7fab6016628e6d3fc0a773c879c228bbb
Suman <Suman@Suman-PC>
2018-08-12 18:30:27 +0530

    PropertyFileReaderUtil.java -> Utility read property file

---
+++ src/main/java/com/AutomationPractice/uiAutomation/Utility/PropertyFileReaderUtil.java
@@ -0,0 +1,27 @@
+package com.AutomationPractice.uiAutomation.Utility;
+
+import java.io.FileInputStream;
+import java.util.Properties;
+
+public class PropertyFileReaderUtil {
+
+	private static Properties prop;
+
+	static {
+
+		prop = new Properties();
+
+		try {
+			FileInputStream fis = new FileInputStream(
+					System.getProperty("user.dir") + "/src/main/resources/configFile/OR.properties");
+			prop.load(fis);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	public static String getPropertyvalue(String key) {
+		return prop.getProperty(key);
+}
+}
