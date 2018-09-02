5a261ee6681e75a412db5702fe7a9dd12126b03d
Suman <Suman@Suman-PC>
2018-09-02 16:29:14 +0530

    updated excel reader code

--- src/main/java/com/AutomationPractice/uiAutomation/Utility/DropDownHelper.java
+++ src/main/java/com/AutomationPractice/uiAutomation/Utility/DropDownHelper.java
@@ -8,17 +8,20 @@ public class DropDownHelper {
 
 	private static final Logger Log = LoggerUtil.getLogger(DropDownHelper.class);
 	private static Select oSelect;
-	
+		
+	public static void selectByIndex(WebElement element, String option) {
+		oSelect = new Select(element);
+		oSelect.selectByVisibleText(option);
+		Log.info("Locator : " + element + " Value : " + option);
+	}
+	public static void selectByValue(WebElement element, String option) {
+		oSelect = new Select(element);
+		oSelect.selectByValue(option);
+		Log.info("Locator : " + element + " Value : " + option);
+	}	
 	public static void selectByVisibleText(WebElement element, String option) {
 		oSelect = new Select(element);
 		oSelect.selectByVisibleText(option);
 		Log.info("Locator : " + element + " Value : " + option);
 	}
-	
-	public static void selectByValue(WebElement element, String option) {
-		oSelect = new Select(element);
-		oSelect.selectByValue(option);
-		Log.info("Locator : " + element + " Value : " + option);
-	}
-	
 }
